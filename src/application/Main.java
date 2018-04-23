c5e9f458969d389280c387e6f457c893ebb8f753
akshatraika1 <36743585+akshatraika1@users.noreply.github.com>
2018-04-22 19:44:44 -0500

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,12 @@
 package application;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -79,78 +81,87 @@ public class Main extends Application {
 		panels.get(panels.size()-1).setCenter(center);
 
 
-		//making Vertical boxes for each left side and right side of each panel.
-		//		for (int i = 0; i < numberOfRounds; i++) {
-		//			
-		//			//TODO add text boxes and labels to all vBoxes according to the number of games to be played in that round.
-		//			panels.get(i).setLeft(leftVBox);
-		//			panels.get(i).setRight(rightVBox);
-		//}
-		//		primaryStage.show();
-		//		}
-		//BorderPane root = new BorderPane();
-		//		            root.setPadding(new Insets(20, 20, 20, 20));
 		Scene scene = new Scene(panels.get(0), 1500, 1000);
-		//		panels.get(0).setStyle("-fx-background-color: linear-gradient(from 0% 15% to 0% 105%, #FFFFFF, #c0c0c0, #ffd700)");
-		//            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		//            
-		//            Label Title = new Label("Tourney Generator 0.1ß");
-		//            Title.setFont(new Font("Comic Sans MS Bold", 72.0));
-
-		//             HBox hbox = new HBox();
-		//             hbox.setPrefSize(600, 400);
-		//            
-		//             TextArea entranceList = new TextArea();
-		//             entranceList.setPrefSize(300, 400);
-		//             entranceList.setPromptText("Add the participants here:");
-		//            
-		//             hbox.getChildren().add(entranceList);
-		//             HBox.setMargin(entranceList, new Insets(10, 10, 10, 10));
-		//            
-		//             final Button fileButton = new Button("Import Text File");
-		//            
-		//             fileButton.setOnAction(new EventHandler<ActionEvent>() {
-		//             @Override
-		//             public void handle(ActionEvent e) {
-		//             System.out.println("Hello World!");
-		//             }
-		//             });
-
-		//             fileButton.setId("bevel-grey");
-
-		// Set Window widgets
-		//            root.setTop(Title);
-		//            BorderPane.setAlignment(Title, Pos.TOP_CENTER);
-		//             root.setBottom(fileButton);
-		//             BorderPane.setAlignment(fileButton, Pos.TOP_CENTER);
-		//             root.setCenter(hbox);
-		//             BorderPane.setAlignment(hbox, Pos.CENTER);
-		//             Sets initial window
 		primaryStage.setScene(scene);
-		//            primaryStage.setTitle("Tourney Generator 0.1ß");
 		primaryStage.show();
-		//        }
-		//        catch (Exception e) {
-		//            e.printStackTrace();
-		//        }
-		//    }
 	}
+	//making Vertical boxes for each left side and right side of each panel.
+	//		for (int i = 0; i < numberOfRounds; i++) {
+	//			
+	//			//TODO add text boxes and labels to all vBoxes according to the number of games to be played in that round.
+	//			panels.get(i).setLeft(leftVBox);
+	//			panels.get(i).setRight(rightVBox);
+	//}
+	//		primaryStage.show();
+	//		}
+	//BorderPane root = new BorderPane();
+	//		            root.setPadding(new Insets(20, 20, 20, 20));
+	//	panels.get(0).setStyle("-fx-background-color: linear-gradient(from 0% 15% to 0% 105%, #FFFFFF, #c0c0c0, #ffd700)");
+	//            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	//            
+	//            Label Title = new Label("Tourney Generator 0.1ÃŸ");
+	//            Title.setFont(new Font("Comic Sans MS Bold", 72.0));
+
+	//             HBox hbox = new HBox();
+	//             hbox.setPrefSize(600, 400);
+	//            
+	//             TextArea entranceList = new TextArea();
+	//             entranceList.setPrefSize(300, 400);
+	//             entranceList.setPromptText("Add the participants here:");
+	//            
+	//             hbox.getChildren().add(entranceList);
+	//             HBox.setMargin(entranceList, new Insets(10, 10, 10, 10));
+	//            
+	//             final Button fileButton = new Button("Import Text File");
+	//            
+	//             fileButton.setOnAction(new EventHandler<ActionEvent>() {
+	//             @Override
+	//             public void handle(ActionEvent e) {
+	//             System.out.println("Hello World!");
+	//             }
+	//             });
+
+	//             fileButton.setId("bevel-grey");
+
+	// Set Window widgets
+	//            root.setTop(Title);
+	//            BorderPane.setAlignment(Title, Pos.TOP_CENTER);
+	//             root.setBottom(fileButton);
+	//             BorderPane.setAlignment(fileButton, Pos.TOP_CENTER);
+	//             root.setCenter(hbox);
+	//             BorderPane.setAlignment(hbox, Pos.CENTER);
+	//             Sets initial window
+	//  }
+	//        catch (Exception e) {
+	//            e.printStackTrace();
+	//        }
+	//    }
+
 	public static void main(String[] args) {
-		//		try {
-		//			List<String> teams = Files.readAllLines(Paths.get(args[0]));
-		//			List<String> act = new ArrayList<String>();
-		//			for (String team : teams) {
-		//				if (!team.trim().isEmpty())
-		//					act.add(team);
-		//			}
-		//			br.parse(act);
-		//			br.generateBracket();
-		//		}
-		//		catch (IOException e) {
-		//			// TODO Auto-generated catch block
-		//			e.printStackTrace();
-		//		}
+
+		File dataFile = new File(args[0]);
+		try
+		{
+			Scanner scnr = new Scanner(dataFile);
+		System.out.println(scnr.nextLine());
+		} catch ( Exception e) {
+
+		}
 		launch(args);
 	}
+	//	try {
+	//			List<String> teams = Files.readAllLines(Paths.get(args[0]));
+	//			List<String> act = new ArrayList<String>();
+	//			for (String team : teams) {
+	//				if (!team.trim().isEmpty())
+	//					act.add(team);
+	//			}
+	//			br.parse(act);
+	//			br.generateBracket();
+	//		}
+	//		catch (IOException e) {
+	//			// TODO Auto-generated catch block
+	//			e.printStackTrace();
+	//		}
 }
 
